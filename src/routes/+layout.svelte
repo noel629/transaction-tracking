94229de5c6ce692f1c552c88008589c25a755fe4
Tommy <tommychinliwei@gmail.com>
2026-03-03 14:23:58 +0800

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import "./layout.css";
+    import favicon from "$lib/assets/favicon.svg";
+    import { page } from "$app/state";
+    import { Navbar, NavLi, NavUl } from "flowbite-svelte";
+
+    let activeUrl = $derived(page.url.pathname);
+    let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<Navbar class="bg-surface border-b-border-warm">
+    <NavUl {activeUrl}>
+        <NavLi href="/dashboard">Dashboard</NavLi>
+        <NavLi href="/transaction">Transaction</NavLi>
+        <NavLi href="/reconciliation">Reconciliation</NavLi>
+        <NavLi href="/settings">Settings</NavLi>
+    </NavUl>
+</Navbar>
+
+<main class="flex-1 flex overflow-hidden px-8 py-8 bg-brand-bg">
+    {@render children()}
+</main>
---
+++ src/routes/transaction/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import { Button } from "flowbite-svelte";
+    import { Table } from "flowbite-svelte";
+
+    const items = [
+        {
+            type: "Type",
+            merchant: "Title",
+            date: "26 December, 1:00 PM",
+            category: "Category",
+            amount: "0.00",
+        },
+    ];
+</script>
+
+<div class="flex h-full w-full overflow-hidden">
+    <div class="flex-1 flex flex-col overflow-y-auto">
+        <div class="flex w-full justify-between items-end mb-8">
+            <div>
+                <h1 class="text-2xl font-medium">Transactions</h1>
+                <p>Manage and review monthly expenses.</p>
+            </div>
+
+            <Button color="light" pill>December 2025</Button>
+        </div>
+
+        <Table
+            {items}
+            shadow
+            hoverable
+            class="shadow-md rounded-full border border-border-warm"
+        ></Table>
+
+        <div class="ml-2">
+            <span class="text-xs"
+                >Showing transaction {items.length} of {items.length}</span
+            >
+        </div>
+    </div>
+</div>
