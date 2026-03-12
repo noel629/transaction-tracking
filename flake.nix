0e92640f6930c41f2a1b6c37da16fd786ba90905
Tommy <tommychinliwei@gmail.com>
2026-03-12 22:38:16 +0800

    dev(nix): add nix flake for pnpm + node + typescript

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "pnpm + nodejs_22 + typescript environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShells."${system}".default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs_22
+        typescript
+        pnpm
+      ];
+
+      shellHook = ''
+        export PS1="\[\e[1;32m\](whatsapp) $PS1"
+      '';
+    };
+  };
+}
